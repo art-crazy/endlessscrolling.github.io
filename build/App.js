57d83d9c42eb725b1519feb2021cebcd037b9ef5
arzhannikov <escudoboot@mail.ru>
2023-10-07 10:53:10 +0700

    -

---
+++ build/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { BrowserRouter as Router, Route,} from 'react-router-dom';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { ReactQueryDevtools } from 'react-query/devtools';
+import PostList from "./pages/PostList";
+import PostDetails from "./pages/PostDetails";
+
+const queryClient = new QueryClient();
+
+const App = () => {
+    return (
+        <QueryClientProvider client={queryClient}>
+            <Router>
+                <Route exact path="/" component={PostList} />
+                <Route path="/post/:id" component={PostDetails} />
+            </Router>
+            <ReactQueryDevtools initialIsOpen={false} />
+        </QueryClientProvider>
+    );
+};
+
+export default App;
